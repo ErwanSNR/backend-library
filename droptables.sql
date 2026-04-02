7a336f4c393e96fa1d79d9710956f026cabaa40d
ErwanSNR <erw4nramadh4n@gmail.com>
2026-04-02 14:15:24 +0700

    fix: move nestjs cli to dependencies

---
+++ droptables.sql
@@ -0,0 +1,1 @@
+DROP TABLE IF EXISTS Peminjaman, Book, Student, User, _prisma_migrations; 
--- prisma/migrations/20260211020012_1_2/migration.sql
+++ prisma/migrations/20260211020012_1_2/migration.sql
@@ -1,8 +1,8 @@
 -- DropForeignKey
-ALTER TABLE `user` DROP FOREIGN KEY `User_studentId_fkey`;
+ALTER TABLE `User` DROP FOREIGN KEY `User_studentId_fkey`;
 
 -- AlterTable
-ALTER TABLE `user` MODIFY `studentId` INTEGER NULL;
+ALTER TABLE `User` MODIFY `studentId` INTEGER NULL;
 
 -- AddForeignKey
 ALTER TABLE `User` ADD CONSTRAINT `User_studentId_fkey` FOREIGN KEY (`studentId`) REFERENCES `Student`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
